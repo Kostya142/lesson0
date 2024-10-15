4b2655e4b3e2fddf63286e9ca0ccf2f187c7ad24
Kostya142 <maestro.vkv@gmail.com>
2024-10-15 12:25:54 +0700

    Add files via upload

---
+++ Home work to.py
@@ -0,0 +1,23 @@
+print("Task №1")
+#1st program
+print("coment - 1st program")
+print("(9**0.5*5)")
+print(9**0.5*5)
+print("Task №2")
+print("coment - 2nd program")
+#2nd program
+print("9.99>9.98 and 1000!=1000.1")
+print(9.99>9.98 and 1000!=1000.1)
+print("Task №3")
+print("coment - 3rd program")
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+print("coment - 4th program")
+print("Task №4")
+#4th program
+print("дано '123.456' преобразуем 'str' ~ 'float' получаем 123.456 ")
+print(float('123.456'),(123.456*10),(int (1234.56))%10)
+print(int(float('123.456')*10)%10)
+

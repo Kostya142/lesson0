dd6111ea7463f62ffa42c85cf8d2402201af4624
Kostya142 <maestro.vkv@gmail.com>
2024-10-20 18:17:02 +0700

    Add files via upload

--- modul_1_control.py
+++ modul_1_control.py
@@ -1,21 +1,15 @@
 from statistics import mean
-#Вариант без возможности ввода с клавиатуры новых имён учеников
+# Вариант без возможности ввода с клавиатуры новых имён учеников
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students=sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik','Aaron' })
-print(students)
-Aaron=mean(grades[0])
-Bilbo=mean(grades[1])
-Johnny=mean(grades[2])
-Khendrik=mean(grades[3])
-Steve=mean(grades[4])
-sr_bal=[Aaron, Bilbo, Johnny, Khendrik, Steve]
+students_=sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik','Aaron' })
+print(students_)
+Aaron_=mean(grades[0])
+Bilbo_=mean(grades[1])
+Johnny_=mean(grades[2])
+Khendrik_=mean(grades[3])
+Steve_=mean(grades[4])
+sr_bal=[Aaron_, Bilbo_, Johnny_, Khendrik_, Steve_]
 print(sr_bal)
 Final=dict(zip(students, sr_bal))
 print(Final)
-
-
-
-
-
-

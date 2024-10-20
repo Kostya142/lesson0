d623120d9045bdf60c9c738a484e1203517f7324
Kostya142 <maestro.vkv@gmail.com>
2024-10-21 00:42:46 +0700

    Add files via upload

---
+++ modul_2_2.py
@@ -0,0 +1,11 @@
+first=24
+second =45
+third=76
+if first==second and second==third:
+    print(3)
+elif first==second or second==third:
+    print(2)
+elif first==third:
+    print(2)
+else:
+    print(0)

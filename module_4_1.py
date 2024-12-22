8038c329dbdc16beba02f60ab7ec028a71fb1887
Kostya142 <maestro.vkv@gmail.com>
2024-12-22 13:35:21 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from true_math import divide as dtr
+from fake_math import divide as dfa
+
+result_1=dtr(1,2)
+result_2=dtr(9,0)
+result_3=dfa(3,4)
+result_4=dfa(3,0)
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)

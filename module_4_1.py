916743ce83148b0a9fd8a22882119b357b38cf27
Kostya142 <maestro.vkv@gmail.com>
2025-01-11 09:30:47 +0700

    Add files via upload

--- module_4_1.py
+++ module_4_1.py
@@ -1,6 +1,16 @@
 from true_math import divide as dtr
 from fake_math import divide as dfa
-
+from math import inf
+def divide(first, second):
+   if second==0:
+      return inf
+   else:
+      return first / second
+def divide(first, second):
+    if second==0:
+       return '<Ошибка>'
+    else:
+        return first / second
 result_1=dtr(1,2)
 result_2=dtr(9,0)
 result_3=dfa(3,4)
